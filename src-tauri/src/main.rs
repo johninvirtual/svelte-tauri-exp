07afa8317c2e720a6464afc01613597f09e969fe
John Marshal <m.johnmarshal@gmail.com>
2022-09-21 20:29:48 +0530

    Test rust function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,16 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn knock(message: &str) -> String {
+    format!("{}, whose there --from rust", message)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![knock])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { invoke } from '@tauri-apps/api';
+
+	let result = '';
+
+	onMount(() => {
+		invoke('knock', { message: 'knock knock' }).then((res) => (result = res));
+	});
+</script>
+
+<div>
+	{result}
+</div>
